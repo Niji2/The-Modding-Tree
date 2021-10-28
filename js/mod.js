eb96028d3bfa94122d94c03ee7f58c80049fea83
Niji <nick@otgeeks.net>
2021-10-28 11:15:47 -0400

    v0.0
    I'm COOOOOOOOOOOOOOOOODINGGGGGGGGGGG

--- js/mod.js
+++ js/mod.js
@@ -1,8 +1,8 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
-	pointsName: "points",
+	name: "The Juicer Tree",
+	id: "JuicerPogKekSama",
+	author: "Sylvia",
+	pointsName: "Juice",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
